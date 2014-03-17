1a1d13336e1eb6f49b137aadcdb48dffc348f6ca
Martin Christov <martin.christov@gmail.com>
2014-03-17 11:24:10 +0200

    added a mobile menu. included angular & hammer.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,39 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+var angular, $win=$(window),$doc=$(document);
+
+angular.module('nazaem',[
+		'hmTouchEvents'
+	]);
+
+
+//controllers
+angular.module('nazaem')
+.controller('MainCtrl', ['$scope', function ($scope) {
+	
+}])
+
+
+//directives
+angular.module('nazaem')
+.directive('calculator', [function () {
+	return {
+		restrict: 'A',
+		link: function ($scope, el, attrs) {
+			
+		}
+	};
+}])
+.
+directive('height100', [function () {
+	return {
+		restrict: 'A',
+		link: function ($scope, el, attrs) {
+			function res(){
+				if($win.width()<=768)
+				$(el).css({height:$doc.height()});
+			}
+			res();
+			$win.resize(res);
+		}
+	};
+}])
