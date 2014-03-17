ba40bb49117e988bfd7e5e3ac0af60927a3d7ac8
Martin Christov <martin.christov@gmail.com>
2014-03-17 15:09:42 +0200

    config changes. images don't get revisioned

--- Gruntfile.js
+++ Gruntfile.js
@@ -203,8 +203,8 @@ module.exports = function (grunt) {
                     src: [
                         '<%= config.dist %>/scripts/{,*/}*.js',
                         '<%= config.dist %>/styles/{,*/}*.css',
-                        '<%= config.dist %>/images/{,*/}*.*',
-                        '<%= config.dist %>/styles/fonts/{,*/}*.*',
+                        // '<%= config.dist %>/images/{,*/}*.*',
+                        // '<%= config.dist %>/styles/fonts/{,*/}*.*',
                         '<%= config.dist %>/*.{ico,png}'
                     ]
                 }
